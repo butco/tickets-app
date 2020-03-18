7bcaf50faedc005180c2277f1ab885f33aa2d155
Marcel Butcovan <butco.marcel@gmail.com>
2020-03-18 21:45:52 +0000

    Created index page

--- index.php
+++ index.php
@@ -1,24 +1,47 @@
 <?php
 
-    include_once "config/Db.php";
+include_once "config/Db.php";
 
-    $conn = new Db();
-    $conn->connect();
+$conn = new Db();
+$conn->connect();
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+<?php include "includes/header.php";?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Ticket App - by ButcoSoft</title>
-</head>
-
-<body>
-
-</body>
-
-</html>
+<div class="container-fluid container-bg">
+    <!-- Image and text -->
+    <!-- <nav class="navbar navbar-light" style="background-color:rgba(0,0,0,0)">
+        <a class="navbar-brand" href="index.php">
+            <img src="" width="30" height="30" class="d-inline-block align-top" alt="">
+            TicketsApp
+        </a>
+    </nav> -->
+    <div class="row">
+        <div class="card card-login">
+            <div class="card-body m-auto">
+                <form>
+                    <h3>TicketsApp</h3>
+                    <div class="form-group">
+                        <label for="emailInput">Email address</label>
+                        <input type="email" class="form-control" id="emailInput" aria-describedby="emailHelp">
+                    </div>
+                    <div class="form-group">
+                        <label for="passInput">Password</label>
+                        <input type="password" class="form-control" id="passInput">
+                    </div>
+                    <!-- <div class="form-group form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                    </div> -->
+                    <button type="submit" class="btn btn-primary btn-block btn-login">Login</button>
+                    <div class="copyright">2020 &copy; ButcoSoft. All rights reserved.</div>
+                </form>
+            </div>
+        </div>
+        <div class="illustration">
+            <img src="images/dev_illustration.svg" alt="" srcset="">
+        </div>
+    </div>
+</div>
+<?php include "includes/footer.php";?>
