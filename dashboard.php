95f7263dec40d8a64e1f66b36ee6c8da9bf977fa
Marcel Butcovan <butco.marcel@gmail.com>
2020-03-27 16:05:26 +0000

    Made the index page to be responsive

--- dashboard.php
+++ dashboard.php
@@ -6,8 +6,16 @@ $users->logged_out_redirect();
 //Save all user details into $user object
 $user = $users->UserDetails($_SESSION['user_id']);
 
+include "includes/header.php";
 ?>
-
-<h2>Welcome, <?php echo $user->user_fullname; ?></h2>
-<br>
-<a href="logout.php">Logout</a>
+<div class="container-fluid container-bg">
+    <div class="row">
+        <div class="col-lg-2 col-md-3 col-sm-4 col-xs-1 sidebar top-fixed">
+            <h2 class="text-white">Welcome, <?php echo $user->user_fullname; ?></h2>
+            <br>
+            <a href="logout.php">Logout</a>
+        </div>
+        <div class="col-lg-10 col-md-9 col-sm-8 col-xs-1"></div>
+    </div>
+</div>
+<?php include "includes/footer.php";?>
--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require "config/init.php";
 
 $users->logged_in_redirect();
 $login_errors = '';
+$email = '';
 
 if (isset($_POST['btnLogin'])) {
     $email = trim($_POST['inputEmail']);
@@ -30,38 +31,42 @@ if (isset($_POST['btnLogin'])) {
 <?php include "includes/header.php";?>
 
 <div class="container-fluid container-bg">
-    <div class="row">
-        <div class="card card-login">
-            <div class="card-body">
-                <form method="POST">
-                    <h3>TicketsApp</h3>
-                    <?php if ($login_errors != ""): ?>
-                    <div class="alert alert-danger" role="alert">
-                        <strong><?=$login_errors;?></strong>
-                    </div>
-                    <?php endif;?>
-                    <div class="form-group">
-                        <label for="emailInput">Email address</label>
-                        <input type="email" class="form-control" id="emailInput" name="inputEmail"
-                            aria-describedby="emailHelp" value=<?=$email;?>>
-                    </div>
-                    <div class="form-group">
-                        <label for="passInput">Password</label>
-                        <input type="password" class="form-control" name="inputPassword" id="passInput">
-                    </div>
-                    <!-- <div class="form-group form-check">
+    <div class="row row-index">
+        <div class="col-lg-6 col-md-6">
+            <div class="card card-login">
+                <div class="card-body">
+                    <form action="index.php" method="POST">
+                        <h3>TicketsApp</h3>
+                        <?php if ($login_errors != ""): ?>
+                        <div class="alert alert-danger" role="alert">
+                            <strong><?=$login_errors;?></strong>
+                        </div>
+                        <?php endif;?>
+                        <div class="form-group">
+                            <label for="emailInput">Email address</label>
+                            <input type="email" class="form-control" id="emailInput" autocomplete="off" autofocus
+                                name="inputEmail" aria-describedby="emailHelp" value="<?php echo $email; ?>">
+                        </div>
+                        <div class="form-group">
+                            <label for="passInput">Password</label>
+                            <input type="password" class="form-control" name="inputPassword" id="passInput">
+                        </div>
+                        <!-- <div class="form-group form-check">
                         <input type="checkbox" class="form-check-input" id="exampleCheck1">
                         <label class="form-check-label" for="exampleCheck1">Check me out</label>
                     </div> -->
-                    <button type="submit" class="btn btn-primary btn-block btn-login" name="btnLogin">Login</button>
-                    <div class="copyright">2020 &copy; <a href="https://www.ButcoSoft.com"
-                            class="copy-link">ButcoSoft</a>. All
-                        rights reserved.</div>
-                </form>
+                        <button type="submit" class="btn btn-primary btn-block btn-login" name="btnLogin">Login</button>
+                        <div class="copyright">2020 &copy; <a href="https://www.ButcoSoft.com"
+                                class="copy-link">ButcoSoft</a>. All
+                            rights reserved.</div>
+                    </form>
+                </div>
             </div>
         </div>
-        <div class="illustration">
-            <img src="images/dev_illustration.svg" alt="" srcset="">
+        <div class="col-lg-6 col-md-6">
+            <div class="illustration">
+                <img src="images/dev_illustration.svg" alt="" srcset="">
+            </div>
         </div>
     </div>
 </div>
